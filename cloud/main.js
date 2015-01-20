ed259811bc947a70361a19ef49df6f4f8b330fd2
mofumofu3n <mofumofu3n@gmail.com>
2015-01-21 00:13:20 +0900

    Feed Dataを取得し、順次RSSにアクセスする

--- cloud/main.js
+++ cloud/main.js
@@ -1,14 +1,44 @@
-// Use Parse.Cloud.define to define as many cloud functions as you want.
+/**
+* 'Feed'の全レコードを取得し、feedUrlカラムに保存されているURLに順次アクセスする
+*/
+
+// jobメソッドの第一引数に指定した名前のジョブが作成される。
 Parse.Cloud.job("lineup", function(request, status) {
-    Parse.Cloud.httpRequest({
-        url: 'http://animemap.net/api/table/tokyo.json',
-        success: function(response) {
-            var body = JSON.parse(response.text);
-            console.log(body);
-            status.success("parse animap");
+    var feedQuery = new Parse.Query("Feed");
+    feedQuery.find({
+        success: function(results) {
+            var length = results.length; 
+
+            // ロックオブジェクト
+            var LOCK = 0;
+            for (var i = 0; i < length; i++) {
+                var feed = results[i];
+                var feedUrl = feed.get('feedUrl');
+                // httpリクエスト
+                Parse.Cloud.httpRequest({
+                    url: feedUrl,
+                    success: function(response) {
+                        console.log(response.text);
+                        LOCK++;
+                        if (LOCK == length) {
+                            // status.successが呼ばれた時点で処理が終了する
+                            status.success("success httpRequest");
+                        }
+                    },
+                    error: function(response) {
+                        console.error(response.status);
+                        LOCK++;
+                        if (LOCK == length) {
+                            status.error("Error Http Request");
+                        }
+                    }
+                });
+            }
         },
-        error: function(response) {
-            status.error(response);
+        error: function(error) {
+            console.error(error.message);
+            status.error(error.message);
         }
     });
+
 });
